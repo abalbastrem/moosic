8091ca077a2f650c7b21f82a72a04ef639389c54
root <root@h17.informatica.escoladeltreball.org>
2018-04-17 21:05:31 +0200

    TODO HTTP GET

---
+++ controller/dbcfg.js
@@ -0,0 +1,10 @@
+const dbcfg = {
+  dbip: '192.168.1.18',
+  dbclient: 'postgresql',
+  dbport: '5432', // postgresql port
+  dbname: 'moosic',
+  dbusername: 'albert',
+  dbpassword: 'jupiter'
+};
+
+module.exports = dbcfg;
--- controller/server.js
+++ controller/server.js
@@ -1,77 +1,122 @@
 /// SERVER CONFIG ///
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-// var bcrypt = require('bcrypt');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const bcrypt = require('bcryptjs');
 // var logger = require('./log');
 // logger.info('error log started');
-var pg = require('pg');
-var pgpromise = require('pg-promise');
+const pg = require('pg');
+const dbCfg = require('./dbcfg');
+// const pgpromise = require('pg-promise');
 server.listen(8888);
 
 console.log("::::: SERVER ONLINE :::::");
 
 /// QUICK LOG ///
 var n = 0;
-var logger = function(request, response, next) {
-  console.log("REQ " + ++n + "\t" + new Date().toISOString());
-  next();
-};
-app.use(logger);
+// var logger = function(request, response, next) {
+//   console.log("REQ " + ++n + "\t" + new Date().toISOString());
+//   next();
+// };
+// app.use(logger);
 
 function log(request, response) {
-  console.log("PATH:\t" + request.route.path);
-  console.log("URL:\t" + request.originalUrl);
-  console.log("IP:\t" + request.ip);
+  console.log("::::: REQ " + ++n + "\t" + new Date().toISOString());
+  console.log("::::: PATH:\t" + request.route.path);
+  console.log("::::: URL:\t" + request.originalUrl);
+  console.log("::::: IP:\t" + request.ip);
   console.log("");
 }
 
 /// DB CONNECTION ///
-var conCfg = {
-  dbip: '192.168.1.18',
-  dbclient: 'postgresql',
-  dbport: '5432', // postgresql port
-  dbname: 'moosic',
-  dbusername: 'albert',
-  dbpassword: 'jupiter'
-};
-var conStr = conCfg.dbclient + '://' + conCfg.dbusername + ':' + conCfg.dbpassword + '@' + conCfg.dbip + ':' + conCfg.dbport + '/' + conCfg.dbname;
-console.log(conStr);
+const dbCfgStr = dbCfg.dbclient + '://' + dbCfg.dbusername + ':' + dbCfg.dbpassword + '@' + dbCfg.dbip + ':' + dbCfg.dbport + '/' + dbCfg.dbname;
+const client = new pg.Client(dbCfgStr);
 
-var client = new pg.Client(conStr);
-
-// for (var i = 0; i < 10; i++) {
 async function testdb() {
-  await client.connect();
-  var res = await client.query("SELECT * FROM genero");
-  console.log(res);
-  await client.end();
+  try {
+    console.log("::::: test query to database");
+    await client.connect();
+    var res = await client.query("SELECT * FROM genero");
+    res.rows.forEach(function(element) {
+      console.log(element.name);
+    })
+    await client.query("CREATE TABLE tablatest(col1 text, col2 text, col3 text)");
+    await client.query("INSERT INTO tablatest(col1, col2, col3) VALUES('pedro','romero','pakistan')");
+    await client.end();
+    console.log("::::: test query finished");
+  } catch (e) {
+    console.error("ERROR: " + e);
+  }
 };
 
-testdb();
-// console.log("::::: CONNECTED TO DATABASE :::::");
-// console.log("CONNEXION " + i);
-// client.query("INSERT INTO ojete(col1, col2, col3) VALUES('pedro','romero','pakistan')");
-// query.on('end', () => {
-// });
-// }
+app.get('/testdb', function(request, response) {
+  log(request, response);
+  testdb();
+});
 
-// const res = client.query('SELECT $1::text as message', ['Hello world!']);
-// console.log(res);
-// client.end();
+// general use
+app.post('/get', function(request, response) {
+  log(request, response);
+  get(args);
+});
 
-// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-//   console.log(err ? err.stack : res.rows[0].message); // Hello World!
-//   console.log("hola");
-//   client.end();
-// });
+/// FROM JAMENDO ///
+const JAMENDO_KEY = "e106f235";
+
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
+
+getJSON('https://www.googleapis.com/freebase/v1/text/en/bob_dylan').then(function(data) {
+  alert('Your Json result is:  ' + data.result); //you can comment this, i used it to debug
+  var res = data.result; //display the result in an HTML element
+}, function(status) { //error detection....
+  alert('Something went wrong.');
+});
+
+async function testdump() {
+  try {
+    $.get("https://api.jamendo.com/v3.0/tracks/?client_id=e106f235&format=jsonpretty&limit=10&include=musicinfo&groupby=artist_id", function(data, status) {
+      console.log(data);
+    }, 'html');
+  } catch (e) {
+    console.error("ERROR: " + e);
+  }
+};
+
+// testdump();
+
 
 /// MODEL ///
 // var tracks = require('tracksmodel');
 
 /// HANDLERS ///
+
+function wrapper(request, response, cb) {
+  console.log("REQ " + ++n + "\t" + new Date().toISOString());
+  console.log("PATH:\t" + request.route.path);
+  console.log("URL:\t" + request.originalUrl);
+  console.log("IP:\t" + request.ip);
+  console.log("");
+  cb();
+}
+
 app.get('/', function(request, response) {
-  log(request, response);
+  // log(request, response);
+  wrapper(request, response, this);
   response.write('hello world');
   response.end();
 });
