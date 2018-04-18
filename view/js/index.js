b481d9e6e57a80ea25a3074438483672c2867801
iaw47941842 <iaw47941842@h16.informatica.escoladeltreball.org>
2018-04-18 20:05:18 +0200

    bootstrap slide menu

--- view/js/index.js
+++ view/js/index.js
@@ -1,11 +1,30 @@
+// Slide menu functions
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "850px";
-    document.getElementById("footer").style.width = "850px";
+    document.getElementById("mySidenav").style.width = "650px";
+    document.getElementById("footer").style.width = "650px";
+    document.getElementById("mySidenavBg").style.width = "650px";
     document.getElementById("icon-open").style.opacity = 0;
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("footer").style.width = "0";
+    document.getElementById("mySidenavBg").style.width = "0";
     document.getElementById("icon-open").style.opacity = 1;
 }
+
+// Waves audio
+
+// var wavesurfer = WaveSurfer.create({
+//     container: '#waveform',
+//     scrollParent: true
+// });
+//
+// wavesurfer.load('audio/id.mp3');
+//
+// wavesurfer.on('ready', function () {
+//     wavesurfer.play();
+// });
+
+// functions of buttons play, pause, forward, backward
