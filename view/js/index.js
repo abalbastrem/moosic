d177b242027a003a313a6ef77d170be0f40a340b
iaw47941842 <iaw47941842@h16.informatica.escoladeltreball.org>
2018-04-18 17:43:04 +0200

    slide menu with footer

---
+++ view/js/index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "850px";
+    document.getElementById("footer").style.width = "850px";
+    document.getElementById("icon-open").style.opacity = 0;
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("footer").style.width = "0";
+    document.getElementById("icon-open").style.opacity = 1;
+}
