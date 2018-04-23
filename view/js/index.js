64744345f1264cec5431694b1a73e7d914d6d2b2
jordi capellades edo <jordiedo29@gmail.com>
2018-04-23 18:46:32 +0200

    buttons track

--- view/js/index.js
+++ view/js/index.js
@@ -1,5 +1,64 @@
-// Slide menu functions
+$(document).ready(function(){
+  // Waves audio
+  var Spectrum = WaveSurfer.create({
+      container: '#audio-spectrum',
+      // Add some color to the audio spectrum
+      progressColor: "#03a9f4",
+      barWidth: 1,
+      skipLength: 20.0
+  });
 
+  Spectrum.on("ready", function(){
+      // Do whatever you need to do with the player
+      $('#play').click(function(){
+        // alert(this.id);
+        if (this.id === 'pause') {
+          // alert('pausando');
+          $('#pause').removeClass('fa fa-pause').addClass('fa fa-play');
+          //$('#play').removeAttr('id', 'pause');
+          $('#pause').attr('id', 'play');
+          Spectrum.pause();
+        } else {
+          $('#play').removeClass('fa fa-play').addClass('fa fa-pause');
+          //$('#play').removeAttr('id', 'play');
+          $('#play').attr('id', 'pause');
+          Spectrum.play();
+        }
+      });
+
+      $('#stop').click(function(){
+        // alert('btn-stop')
+        $('#pause').removeClass('fa fa-pause').addClass('fa fa-play');
+        $('#play').removeAttr('id', 'pause');
+        $('#pause').attr('id', 'play');
+        Spectrum.stop();
+      });
+
+      $('#forward').click(function(){
+        // alert('btn-forward')
+        Spectrum.skipForward();
+      });
+
+      $('#backward').click(function(){
+        // alert('btn-backward')
+        Spectrum.skipBackward();
+      });
+  });
+
+  Spectrum.load('https://mp3l.jamendo.com/?trackid=1501986&format=mp31&from=app-e106f235');
+});
+
+// Button object
+
+var buttons = {
+    play: document.getElementById("play"),
+    pause: document.getElementById("pause"),
+    pause: document.getElementById("stop"),
+    backward: document.getElementById("backward"),
+    forward: document.getElementById("forward")
+};
+
+// Slide menu functions
 function openNav() {
     document.getElementById("mySidenav").style.width = "650px";
     document.getElementById("footer").style.width = "650px";
@@ -13,33 +72,3 @@ function closeNav() {
     document.getElementById("mySidenavBg").style.width = "0";
     document.getElementById("icon-open").style.opacity = 1;
 }
-
-// Waves audio
-
-$('document').ready(function(){
-  var Spectrum = WaveSurfer.create({
-      container: '#audio-spectrum',
-      // Add some color to the audio spectrum
-      progressColor: "#03a9f4"
-  });
-
-  Spectrum.on("ready", function(){
-      // Do something when the file has been loaded
-
-      // Do whatever you need to do with the player
-      Spectrum.play();
-      Spectrum.pause();
-      Spectrum.stop();
-  });
-
-  Spectrum.load('https://mp3l.jamendo.com/?trackid=1501986&format=mp31&from=app-e106f235');
-});
-
-
-
-
-// wavesurfer.on('ready', function () {
-//     wavesurfer.play();
-// });
-
-// functions of buttons play, pause, forward, backward
