e515a2f4bdaccd4324316c384467f1e177f440aa
abalbastrem <bulbastre@gmail.com>
2018-04-25 20:09:46 +0200

    dump NOW

--- controller/connection.js
+++ controller/connection.js
@@ -1,9 +1,9 @@
-const baix = require('pg');
+const pg = require('pg');
 // var pgp = require('pg-promise')();
 // const GLOBALS = require('./setup');
 // console.log(GLOBALS.DBCONFIG);
 
-const dbCfg = {
+const dbCon = {
   host: '192.168.1.18',
   type: 'postgresql',
   port: 5432, // postgresql port
@@ -12,15 +12,16 @@ const dbCfg = {
   password: 'jupiter'
 };
 
-const dbConStr = dbCfg.type + '://' + dbCfg.user + ':' + dbCfg.password + '@' + dbCfg.host + ':' + dbCfg.port + '/' + dbCfg.database;
+const dbConStr = dbCon.type + '://' + dbCon.user + ':' + dbCon.password + '@' + dbCon.host + ':' + dbCon.port + '/' + dbCon.database;
 
-//PG
-// exports.client = new pg.Client(dbConStr);
+// PG - for initial and weekly dump
+var pgClient = new pg.Client(dbConStr);
+pgClient.connect();
+exports.pgClient = pgClient;
+console.log("::::: PG CLIENT CONNECTED :::::");
 
-// PGPROMISE
-// var pg = pgp(dbCfg);
-var client = new baix.Client(dbConStr);
-client.connect();
+// PGPROMISE - for user interaction from views
+// var pgpClient = pgp(dbCfg);
 
 // pg.connect()
 //   .then(obj => {
@@ -34,38 +35,36 @@ client.connect();
 // TESTCHECK
 exports.pgptest = async function() {
   for (let i = 0; i < 3; i++) {
-    var res = await pg.query('SELECT now()');
+    var res = await pgpClient.query('SELECT now()');
     console.log(res);
-    var res = await pg.query('SELECT $1:name FROM $2:name', ['id_track', 'tags']);
+    var res = await pgpClient.query('SELECT $1:name FROM $2:name', ['id_track', 'tags']);
     console.log(res);
   }
 };
 
-exports.clienttest = async function() {
+exports.pgtest = async function() {
   // var res = await pg.query('SELECT now()');
   // console.log(res);
   // var res = await pg.query('SELECT $1:name FROM $2:name', ['id_track', 'tags']);
-  for (let i = 0; i < 3; i++) {
-  var res = await client.query('SELECT id_track FROM tags');
+  var res = await pgClient.query('SELECT id_track FROM tags LIMIT 3');
   console.log(res.rows);
-  }
   // await sleep();
   // var res = await pg.query('SELECT $1:name FROM $2:name', ['id_leyenda_tag', 'tags']);
   // console.log(res);
 
 };
 
-
-function sleep() {
-  pgpTest();
-  setTimeout(function() {
-    pgpTest();
-    console.log("sleeping...");
-  }, 5000)
-};
+//
+// function sleep() {
+//   pgpTest();
+//   setTimeout(function() {
+//     pgpTest();
+//     console.log("sleeping...");
+//   }, 5000)
+// };
 
 
 
 // exports.pgp = client;
-exports.client = client;
+// exports.client = client;
 // module.exports = pg;
