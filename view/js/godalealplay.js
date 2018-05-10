3b1652f6fa90d961d867007c213ed68b7dfba952
jmenesesl <jhonnymeneses13@gmail.com>
2018-05-10 20:20:25 +0200

    play songs

---
+++ view/js/godalealplay.js
@@ -0,0 +1,238 @@
+var genres = new Array("pop", "rock", "electronic", "hiphop", "jazz", "indie", "soundtrack", "classical", "chillout", "ambient", "folk", "metal", "latina", "rnb", "reggae", "punk", "country", "house", "blues");
+var index = 0;
+
+$(document).ready(function() {
+  // Go.js
+  if (window.goSamples) goSamples(); // init for these samples -- you don't need to call this
+  var $ = go.GraphObject.make; // for conciseness in defining templates
+
+  var blues = ['#E1F5FE', '#B3E5FC', '#81D4FA', '#4FC3F7', '#29B6F6', '#03A9F4', '#039BE5', '#0288D1', '#0277BD', '#01579B'];
+
+  var myDiagram =
+    $(go.Diagram, "myDiagramDiv", {
+      initialAutoScale: go.Diagram.UniformToFill,
+      contentAlignment: go.Spot.Center,
+      layout: $(go.ForceDirectedLayout),
+      // moving and copying nodes also moves and copies their subtrees
+      "commandHandler.copiesTree": false, // for the copy command
+      "commandHandler.deletesTree": false, // for the delete command
+      "draggingTool.dragsTree": false, // dragging for both move and copy
+      "undoManager.isEnabled": false
+    });
+
+  var myModel = $(go.Model);
+  myDiagram.model = myModel;
+
+  // Define the Node template.
+  // This uses a Spot Panel to position a button relative
+  // to the ellipse surrounding the text.
+  myDiagram.nodeTemplate =
+    $(go.Node, "Spot", {
+        selectionObjectName: "PANEL",
+        isTreeExpanded: false,
+        isTreeLeaf: false
+      },
+      // the node's outer shape, which will surround the text
+      $(go.Panel, "Auto", {
+          name: "PANEL"
+        },
+        $(go.Shape, "Circle", {
+            // width: 60,
+            // height: 60,
+            fill: "whitesmoke",
+            stroke: "black"
+          },
+          new go.Binding("fill", "rootdistance", function(dist) {
+            dist = Math.min(blues.length - 1, dist);
+            return blues[dist];
+          })),
+        $(go.TextBlock, {
+            font: "12pt Roboto",
+            margin: 10
+          },
+          new go.Binding("text", "name"))
+      ),
+      // the expand/collapse button, at the center corner
+      $("TreeExpanderButton", {
+        name: 'TREEBUTTON',
+        width: 40,
+        height: 40,
+        alignment: go.Spot.Center,
+        alignmentFocus: go.Spot.Center,
+        opacity: 0,
+        // customize the expander behavior to
+        // create children if the node has never been expanded
+        click: function(e, obj) { // OBJ is the Button
+          // console.log(e);
+          // console.log(obj);
+          var node = obj.part; // get the Node containing this Button
+          // console.log(node);
+          key = node.data.key;
+          console.log(node.data.key);
+          // console.log(node.data.name);
+          //console.log(node);
+          if (node === null) return;
+          e.handled = true;
+          if (key == 0) {
+            expandNode(node);
+          } else {
+            // method getTags()
+            getTracks(new Array(key));
+            getTags(key);
+            expandNodeTag(node);
+
+          }
+        }
+      }) // end TreeExpanderButton
+    ); // end Node
+
+  async function getTagAsync(tag) {
+    var tags = await getTags(tag);
+    return tags;
+  }
+
+  // create the model with a root node data
+  key = 0;
+  myDiagram.model = new go.TreeModel([{
+    key: 0,
+    name: "TAGS",
+    id: key,
+    color: blues[0],
+    everExpanded: false
+  }]);
+
+  function expandNodeTag(node) {
+    var diagram = node.diagram;
+    diagram.startTransaction("CollapseExpandTree");
+    // this behavior is specific to this incrementalTree sample:
+    var data = node.data;
+    // console.log(data);
+    if (!data.everExpanded) {
+      // console.log("HOLA");
+      // only create children once per node
+      diagram.model.setDataProperty(data, "everExpanded", true);
+
+      var numchildren = createSubTreeTags(data);
+      console.log(numchildren);
+      // console.log(numchildren);
+      if (numchildren === 0) { // now known no children: don't need Button!
+        node.findObject('TREEBUTTON').visible = false;
+      }
+    }
+    // this behavior is generic for most expand/collapse tree buttons:
+    if (node.isTreeExpanded) {
+      diagram.commandHandler.collapseTree(node);
+    } else {
+      diagram.commandHandler.expandTree(node);
+    }
+    diagram.commitTransaction("CollapseExpandTree");
+    // myDiagram.zoomToFit();
+  }
+
+
+  function expandNode(node) {
+    var diagram = node.diagram;
+    diagram.startTransaction("CollapseExpandTree");
+    // this behavior is specific to this incrementalTree sample:
+    var data = node.data;
+    // console.log(data);
+    if (!data.everExpanded) {
+      // only create children once per node
+      diagram.model.setDataProperty(data, "everExpanded", true);
+      // console.log(data);
+      var numchildren = createSubTree(data);
+      // console.log(numchildren);
+      if (numchildren === 0) { // now known no children: don't need Button!
+        node.findObject('TREEBUTTON').visible = false;
+      }
+    }
+    // this behavior is generic for most expand/collapse tree buttons:
+    if (node.isTreeExpanded) {
+      diagram.commandHandler.collapseTree(node);
+    } else {
+      diagram.commandHandler.expandTree(node);
+    }
+    diagram.commitTransaction("CollapseExpandTree");
+    // myDiagram.zoomToFit();
+  }
+
+  // This dynamically creates the immediate children for a node.
+  // The sample assumes that we have no idea of whether there are any children
+  // for a node until we look for them the first time, which happens
+  // upon the first tree-expand of a node.
+  function createSubTree(parentdata) {
+    console.log(top_tags);
+    var numchildren = top_tags.length - 1;
+    // console.log(numchildren);
+    // console.log(numchildren);
+    if (myDiagram.nodes.count <= 1) {
+      numchildren += 1; // make sure the root node has at least one child
+    }
+    // create several node data objects and add them to the model
+    var model = myDiagram.model;
+    var parent = myDiagram.findNodeForData(parentdata);
+
+    var degrees = 1;
+    var grandparent = parent.findTreeParentNode();
+    while (grandparent) {
+      degrees++;
+      grandparent = grandparent.findTreeParentNode();
+    }
+
+    for (var i = 0; i < numchildren; i++) {
+      var childdata = {
+        key: top_tags[index],
+        name: (top_tags[index]).toUpperCase(),
+        parent: parentdata.key,
+        rootdistance: degrees
+      };
+      index++;
+      // add to model.nodeDataArray and create a Node
+      model.addNodeData(childdata);
+      // position the new child node close to the parent
+      var child = myDiagram.findNodeForData(childdata);
+      child.location = parent.location;
+    }
+    return numchildren;
+  }
+
+  function createSubTreeTags(parentdata) {
+    console.log("hola");
+    console.log("algo " + this);
+    // $(this).css("background-color", "gray");
+    console.log(more_tags);
+    var numchildren = more_tags.length - 1;
+    console.log(numchildren);
+    // console.log(numchildren);
+    if (myDiagram.nodes.count <= 1) {
+      numchildren += 1; // make sure the root node has at least one child
+    }
+    // create several node data objects and add them to the model
+    var model = myDiagram.model;
+    var parent = myDiagram.findNodeForData(parentdata);
+
+    var degrees = 1;
+    var grandparent = parent.findTreeParentNode();
+    while (grandparent) {
+      degrees++;
+      grandparent = grandparent.findTreeParentNode();
+    }
+
+    for (var i = 0; i < numchildren; i++) {
+      var childdata = {
+        key: more_tags[index],
+        name: more_tags[index],
+        parent: parentdata.key,
+        rootdistance: degrees
+      };
+      index++;
+      // add to model.nodeDataArray and create a Node
+      model.addNodeData(childdata);
+      // position the new child node close to the parent
+      var child = myDiagram.findNodeForData(childdata);
+      child.location = parent.location;
+    }
+    return numchildren;
+  }
+
+});
---
+++ view/js/methodsdalealplay.js
@@ -0,0 +1,258 @@
+var port = 8888;
+url = "http://192.168.1.17"
+var url_login = url + ":" + port + "/login";
+var url_signup = url + ":" + port + "/signup";
+var url_getMoosics = url + ":" + port + "/getmoosics";
+var url_blindStart = url + ":" + port + "/blindstart";
+var url_tags = url + ":" + port + "/gettags";
+var top_tags = new Array();
+var more_tags = new Array();
+
+
+$(document).ready(function() {
+  // Login
+  $('#signin').click(function() {
+    var email = $('input[id=email]').val();
+    var password = $('input[id=password]').val();
+    var params = {
+      "username": email,
+      "password": password
+    };
+
+    if (email != '' && password != '') {
+      var options = {
+        url: url_login,
+        dataType: "json",
+        type: "POST",
+        data: "json=" + JSON.stringify(params),
+        processData: true,
+        success: function(data) {
+          console.log('success');
+          console.log(data);
+          closeNav();
+          closeNavMenu();
+          //data = JSON.parse(data);
+          // alert(data.data['name'] + " " + data.data['lastname']);
+          // $('#promptTags').removeClass('hide');
+          // promptTags();
+          $('#register').addClass('hide');
+        },
+        error: function(data) {
+          console.log('error');
+        }
+      };
+      $.ajax(options);
+    } else {
+      if (email == '') {
+        $('#emailLoginDiv').addClass('required');
+      } else {
+        $('#emailLoginDiv').removeClass('required');
+      }
+      if (password == '') {
+        $('#passwordLoginDiv').addClass('required');
+      } else {
+        $('#passwordLoginDiv').removeClass('required');
+      }
+      $('#loginErrorEmptyFields').removeClass('hide');
+    }
+  });
+
+  // Sign up
+  $('#signup').click(function() {
+    var name = ($('input[id=name]').val()).trim();
+    var lastname = ($('input[id=lastname]').val()).trim();
+    var username = ($('input[id=username-signup]').val()).trim();
+    var email = ($('input[id=email-signup]').val()).trim();
+    var password = ($('input[id=password-signup]').val()).trim();
+    var repeatPassword = ($('input[id=repeat-password]').val()).trim();
+    var sex = $('select[id=sex]').val();
+    var terms = $('input[id=agreeTerms]').is(':checked');
+
+    //console.log(name + "," + lastname + "," + username + "," + email + "," + password + "," + repeatPassword + "," + sex + "," + terms);
+    var params = {
+      "name": name,
+      "lastname": lastname,
+      "username": username,
+      "email": email,
+      "password": password,
+      "sex": sex
+    }
+    console.log(params);
+
+    validSignUp = validateSignUp(params, terms, repeatPassword);
+    console.log(validSignUp);
+    if (validSignUp == true) {
+      console.log("login valido");
+      var options = {
+        url: url_signup,
+        dataType: "json",
+        type: "POST",
+        data: "json=" + JSON.stringify(params),
+        processData: true,
+        success: function(data) {
+          console.log('success');
+          console.log(data.message);
+
+          if (data.message == 'user already exists') {
+            $('#loginErrorMsgUserAlreadyExists').removeClass('hide');
+          } else {
+            $('#loginErrorMsgUserAlreadyExists').addClass('hide');
+          }
+
+        },
+        error: function(data) {
+          console.log('error');
+          // console.log(data);
+        }
+      };
+      $.ajax(options);
+    } else {
+      $('#loginErrorMsgUserAlreadyExists').addClass('hide');
+      console.log("login no valido");
+    }
+  });
+  // end document ready
+});
+
+function validateSignUp(params, terms, repeatPassword) {
+  console.log(name);
+  var validSignUp = true;
+
+  // if terms not checked
+  if (!terms) {
+    console.log("not checked");
+    $('#loginErrorMsgAgreeTerms').removeClass('hide');
+    validSignUp = false;
+  } else {
+    $('#loginErrorMsgAgreeTerms').addClass('hide');
+  }
+
+  if (params.password != repeatPassword) {
+    console.log("password no identical");
+    $('#loginErrorMsgRepeatPass').removeClass('hide');
+    validSignUp = false;
+  } else {
+    $('#loginErrorMsgRepeatPass').addClass('hide');
+  }
+
+  // required camps red
+  if (params.name == '') {
+    $('#nameDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#nameDiv').removeClass('required');
+  }
+  if (params.lastname == '') {
+    $('#lastnameDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#lastnameDiv').removeClass('required');
+  }
+  if (params.username == '') {
+    $('#usernameDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#usernameDiv').removeClass('required');
+  }
+  if (params.email == '') {
+    $('#emailDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#emailDiv').removeClass('required');
+  }
+  if (params.password == '') {
+    $('#passwordDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#passwordDiv').removeClass('required');
+  }
+  if (repeatPassword == '') {
+    $('#repeatPasswordDiv').addClass('required');
+    validSignUp = false;
+  } else {
+    $('#repeatPasswordDiv').removeClass('required');
+  }
+
+  return validSignUp;
+}
+
+function promptTags() {
+  $('#submitTags').click(function() {
+    var pop = $('input[id=pop]').val();
+    var trap = $('input[id=trap]').val();
+    var rock = $('input[id=rock]').val();
+    console.log(pop);
+    console.log(rock);
+    console.log(trap);
+    $('#promptTags').addClass('hide');
+  });
+}
+
+function getTracks(tagsArray) {
+  var tags = {
+    "tags": tagsArray
+  };
+  var options = {
+    url: url_getMoosics,
+    dataType: "json",
+    type: "POST",
+    data: 'json=' + JSON.stringify(tags),
+    processData: true,
+    success: function(data) {
+      console.log('success');
+      console.log(data);
+      playSongs(data);
+    },
+    error: function(data) {
+      console.log('error');
+      console.log(data);
+    }
+  };
+  $.ajax(options);
+}
+
+function getTags(tag) {
+  var args = {
+    "tags": [tag]
+  };
+
+  var options = {
+    url: url_tags,
+    dataType: "json",
+    type: "POST",
+    data: 'json=' + JSON.stringify(args),
+    processData: true,
+    success: function(data) {
+      console.log('success');
+      more_tags = data.data.slice(0,10);
+      console.log(more_tags);
+    },
+    error: function(data) {
+      console.log('error');
+      console.log(data);
+    }
+  };
+  $.ajax(options);
+}
+
+// top tags
+(function getTopTags() {
+  var port = 8888;
+  var options = {
+    url: url_blindStart,
+    type: "POST",
+    success: function(data) {
+      console.log('success');
+      console.log(data);
+      // top_tags = JSON.parse(data.data);
+      top_tags = data.data.slice(0,10);
+      // console.log(top_tags);
+    },
+    error: function(data) {
+      console.log('error');
+      console.log(data);
+    }
+  };
+  $.ajax(options);
+})()
+;
